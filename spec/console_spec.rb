7685c126fc4d42d8852d83b880758667cccfb23f
Andrew Novikov <andruha2921@gmail.com>
2016-05-31 12:50:57 +0300

    spec refactor

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -2,70 +2,70 @@ require 'spec_helper'
 
 module Codebreaker
   RSpec.describe Console do
-    let(:console ) {subject}
+
     context '#play' do
       before do
-        allow(console).to receive(:gets).and_return('an','1234')
+        allow(subject).to receive(:gets).and_return('an','1234')
       end
       it 'shoud write welcome' do
-        expect {console.play}.to output(/welcome/i).to_stdout
+        expect {subject.play}.to output(/welcome/i).to_stdout
       end
       it 'shoud ask name' do
-        expect {console.play}.to output(/name/).to_stdout
+        expect {subject.play}.to output(/name/).to_stdout
       end
       it 'shoud save name into the game' do
-        allow(console).to receive(:puts)
-        console.play
-        expect(console.game.name).to eq 'an'
+        allow(subject).to receive(:puts)
+        subject.play
+        expect(subject.game.name).to eq 'an'
       end
     end
 
     context '#new_game' do
       before(:each) do
-        allow(console.game).to receive(:status).and_return(:play,:win)
-        allow(console).to receive(:gets) {'no'}
-        allow(console).to receive(:get_choice).and_return(:win)
+        allow(subject.game).to receive(:status).and_return(:play,:win)
+        allow(subject).to receive(:gets) {'no'}
+        allow(subject).to receive(:get_choice).and_return(:win)
       end
       it 'show message You win! when you win', :wined do
-        expect {console.new_game}.to output(/win/i).to_stdout
+        expect {subject.new_game}.to output(/win/i).to_stdout
       end
       it 'show message game over when you lose' do
-        allow(console).to receive(:get_choice) {:lose}
-        expect {console.new_game}.to output(/lose/i).to_stdout
+        allow(subject).to receive(:get_choice) {:lose}
+        expect {subject.new_game}.to output(/lose/i).to_stdout
       end
       it 'propose to save and play again' do
-        allow(console).to receive(:puts)
-        expect(console).to receive(:ask).with(:save?).once
-        expect(console).to receive(:ask).with(:play_again?).once
-        console.new_game
+        allow(subject).to receive(:puts)
+        is_expected.to receive(:ask).with(:save?).once
+        is_expected.to receive(:ask).with(:play_again?).once
+        subject.new_game
       end
       it 'when play again start new game' do
-        allow(console).to receive(:puts)
-        allow(console).to receive(:gets).and_return('yes','yes','no')
-        expect(console).to receive(:new_game).and_call_original.twice
-        console.new_game
+        allow(subject).to receive(:puts)
+        allow(subject).to receive(:gets).and_return('yes','yes','no')
+        is_expected.to receive(:new_game).and_call_original.twice
+        subject.new_game
       end
     end
 
     context '#get_choice' do
       it 'propose to enter choice' do
-        allow(console).to receive(:puts)
-        expect(console).to receive(:gets){'q'}.once
-        console.get_choice
+        allow(subject).to receive(:puts)
+        is_expected.to receive(:gets){'q'}.once
+        subject.get_choice
       end
       it 'when h or hint take a hint' do
-        allow(console).to receive(:gets){'h'}.once
-        expect(console.game).to receive(:hint)
-        expect{console.get_choice}.to output(/\d hints/).to_stdout
+        allow(subject).to receive(:gets){'h'}.once
+        expect(subject.game).to receive(:hint)
+        expect{subject.get_choice}.to output(/\d hints/).to_stdout
       end
       it 'when q or quit game over' do
-        allow(console).to receive(:puts)
-        allow(console).to receive(:gets){'q'}.once
-        expect(console.get_choice).to eq :lose
+        allow(subject).to receive(:puts)
+        allow(subject).to receive(:gets){'q'}.once
+        expect(subject.get_choice).to eq :lose
       end
       it 'when valide code puts answer' do
-        allow(console).to receive(:gets){'1234'}.once
-        expect{console.get_choice}.to output(/answer/).to_stdout
+        allow(subject).to receive(:gets){'1234'}.once
+        expect{subject.get_choice}.to output(/answer/).to_stdout
       end
     end
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,34 +2,34 @@ require 'spec_helper'
 
 module Codebreaker
   RSpec.describe Game do
-    let(:game) {subject}
+
     describe '#start' do
       it 'generates a secret code' do
-        expect(game.instance_variable_get(:@code)).not_to be_empty
+        expect(subject.instance_variable_get(:@code)).not_to be_empty
       end
       it 'set status to :play' do
-        expect(game.status).to eq :play
+        expect(subject.status).to eq :play
       end
     end
 
     describe "#generate" do
       it 'sectet code with 4 numbers' do
-        expect(game.instance_variable_get(:@code).size).to eq 4
+        expect(subject.instance_variable_get(:@code).size).to eq 4
       end
       it 'secret code with numbers 1 to 6' do
-        expect(game.valid_code? game.generate).to eq true
+        expect(subject.valid_code? subject.generate).to eq true
       end
       it 'different codes each time' do
-        expect(game.generate).not_to eq game.generate
+        expect(subject.generate).not_to eq subject.generate
       end
     end
 
     describe '#game_over' do
       it 'return :lose with any args' do
-        expect(game.game_over).to eq :lose
+        expect(subject.game_over).to eq :lose
       end
       it 'return :win with arg' do
-        expect(game.game_over true).to eq :win
+        expect(subject.game_over true).to eq :win
       end
     end
 
@@ -43,32 +43,32 @@ module Codebreaker
 
       variants.each do |variant|
         it "must return #{variant[1]} when guess = #{variant[0]}" do
-          expect(game.check variant[0]).to eq variant[1]
+          expect(subject.check variant[0]).to eq variant[1]
         end
       end
 
       it 'decrease attemps number' do
-        expect {game.check "1232"}.to change {game.attempts}.by(-1)
+        expect {subject.check "1232"}.to change {subject.attempts}.by(-1)
       end
       it 'answer with lose if run out attempt' do
-        allow(game).to receive(:attempts).and_return(0)
-        expect(game.check "4444").to eq :lose
+        allow(subject).to receive(:attempts).and_return(0)
+        expect(subject.check "4444").to eq :lose
       end
     end
 
     describe '#hint' do
       it 'give one number' do
-        expect(game.hint.size).to eq 1
+        expect(subject.hint.size).to eq 1
       end
       it 'contains in secret code' do
-        expect(game.instance_variable_get(:@code)).to include game.hint
+        expect(subject.instance_variable_get(:@code)).to include subject.hint
       end
     end
 
     describe "#save" do
       around do |example|
         DUMP_FILE ||= 'dump'
-        SCORE_FILE_NAME ||= game.default[:score_file_name]
+        SCORE_FILE_NAME ||= subject.default[:score_file_name]
         if File.exist? SCORE_FILE_NAME
           File.write(DUMP_FILE,File.read(SCORE_FILE_NAME))
         end
@@ -81,13 +81,13 @@ module Codebreaker
         end
       end
       it "shoud create file" do
-        game.instance_variable_set(:@name, 'test')
-        game.save
+        subject.instance_variable_set(:@name, 'test')
+        subject.save
         expect(File.exist? SCORE_FILE_NAME).to eq true
       end
       it 'shoud change size by 1' do
-        game.instance_variable_set(:@name, 'test')
-        expect {game.save}.to change {File.read(SCORE_FILE_NAME).lines.count}.by(1)
+        subject.instance_variable_set(:@name, 'test')
+        expect {subject.save}.to change {File.read(SCORE_FILE_NAME).lines.count}.by(1)
       end
     end
   end
